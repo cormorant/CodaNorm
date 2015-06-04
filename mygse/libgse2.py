8df8c26a99c8afde88df6d7a4c86d8182c7a7e42
Peter Forever <crmpeter@gmail.com>
2015-06-04 11:08:59 +0800

    Remade code in mygse dir. Should work

--- mygse/libgse2.py
+++ mygse/libgse2.py
@@ -1,32 +1,11 @@
 #!/usr/bin/env python
-# --------------------------------------------------------------------
-# Filename: libgse2.py
-#  Purpose: Python wrapper for gse_functions of Stefan Stange
-#   Author: Moritz Beyreuther
-#    Email: moritz.beyreuther@geophysik.uni-muenchen.de
-#
-# Copyright (C) 2008-2012 Moritz Beyreuther
+# Python wrapper for gse_functions of Stefan Stange
 # ---------------------------------------------------------------------
 """
 Lowlevel module internally used for handling GSE2 files.
-
-Python wrappers for gse_functions - The GSE2 library of Stefan Stange.
-Currently CM6 compressed GSE2 files are supported, this should be
-sufficient for most cases. Gse_functions is written in C and
-interfaced via python-ctypes.
-
-See: http://www.orfeus-eu.org/software/seismo_softwarelibrary.html#gse
-
-:copyright:
-    The ObsPy Development Team (devs@obspy.org)
-:license:
-    GNU Lesser General Public License, Version 3
-    (http://www.gnu.org/copyleft/lesser.html)
 """
-from __future__ import (absolute_import, division, print_function,
-                        unicode_literals)
-from future.builtins import *  # NOQA
-from future.utils import native_str
+from __future__ import division
+#from future.utils import native_str
 
 import ctypes as C
 import doctest
@@ -34,41 +13,42 @@ import warnings
 
 import numpy as np
 
-from obspy import UTCDateTime
+from mygse.obspycore.utcdatetime import UTCDateTime
 from obspy.core.util.libnames import _load_CDLL
 
 
 # Import shared libgse2
-clibgse2 = _load_CDLL("gse2")
+clibgse2 = C.CDLL("libgse2.pyd")
+
 
 clibgse2.decomp_6b_buffer.argtypes = [
     C.c_int,
     np.ctypeslib.ndpointer(dtype=np.int32, ndim=1,
-                           flags=native_str('C_CONTIGUOUS')),
+                           flags=str('C_CONTIGUOUS')),
     C.CFUNCTYPE(C.c_char_p, C.POINTER(C.c_char), C.c_void_p), C.c_void_p]
 clibgse2.decomp_6b_buffer.restype = C.c_int
 
 clibgse2.rem_2nd_diff.argtypes = [
     np.ctypeslib.ndpointer(dtype=np.int32, ndim=1,
-                           flags=native_str('C_CONTIGUOUS')),
+                           flags=str('C_CONTIGUOUS')),
     C.c_int]
 clibgse2.rem_2nd_diff.restype = C.c_int
 
 clibgse2.check_sum.argtypes = [
     np.ctypeslib.ndpointer(dtype=np.int32, ndim=1,
-                           flags=native_str('C_CONTIGUOUS')),
+                           flags=str('C_CONTIGUOUS')),
     C.c_int, C.c_int32]
 clibgse2.check_sum.restype = C.c_int  # do not know why not C.c_int32
 
 clibgse2.diff_2nd.argtypes = [
     np.ctypeslib.ndpointer(dtype=np.int32, ndim=1,
-                           flags=native_str('C_CONTIGUOUS')),
+                           flags=str('C_CONTIGUOUS')),
     C.c_int, C.c_int]
 clibgse2.diff_2nd.restype = C.c_void_p
 
 clibgse2.compress_6b_buffer.argtypes = [
     np.ctypeslib.ndpointer(dtype=np.int32, ndim=1,
-                           flags=native_str('C_CONTIGUOUS')),
+                           flags=str('C_CONTIGUOUS')),
     C.c_int,
     C.CFUNCTYPE(C.c_int, C.c_char)]
 clibgse2.compress_6b_buffer.restype = C.c_int
@@ -88,14 +68,6 @@ class GSEUtiError(Exception):
     pass
 
 
-# example header of tests/data/loc_RNON20040609200559.z:
-#
-# WID2 2009/05/18 06:47:20.255 RNHA  EHN      CM6      750  200.000000
-# 0123456789012345678901234567890123456789012345678901234567890123456789
-# 0         10        20        30        40        50        60
-#  9.49e-02   1.000    M24  -1.0 -0.0
-# 0123456789012345678901234567890123456789012345678901234567890123456789
-# 70        80        90        100
 def _str(s):
     return s.strip()
 
@@ -126,24 +98,15 @@ GSE2_FIELDS = [
 def isGse2(f):
     """
     Checks whether a file is GSE2 or not. Returns True or False.
-
-    :type f: file
-    :param f: file pointer to start of GSE2 file to be checked.
     """
     pos = f.tell()
     widi = f.read(4)
     f.seek(pos)
-    if widi != b'WID2':
-        raise TypeError("File is not in GSE2 format")
+    if widi != b'WID2': raise TypeError("File is not in GSE2 format")
 
 
 def readHeader(fh):
-    """
-    Reads GSE2 header from file pointer and returns it as dictionary.
-
-    The method searches for the next available WID2 field beginning from the
-    current file position.
-    """
+    """ Reads GSE2 header from file pointer and returns it as dictionary """
     # search for WID2 field
     line = fh.readline()
     while line:
@@ -187,20 +150,7 @@ def readHeader(fh):
 
 
 def writeHeader(f, headdict):
-    """
-    Rewriting the write_header Function of gse_functions.c
-
-    Different operating systems are delivering different output for the
-    scientific format of floats (fprintf libc6). Here we ensure to deliver
-    in a for GSE2 valid format independent of the OS. For speed issues we
-    simple cut any number ending with E+0XX or E-0XX down to E+XX or E-XX.
-    This fails for numbers XX>99, but should not occur.
-
-    :type f: file
-    :param f: File pointer to to GSE2 file to write
-    :type headdict: dict
-    :param headdict: ObsPy header
-    """
+    """ Rewriting the write_header Function of gse_functions.c """
     calib = "%10.2e" % (headdict['calib'])
     date = headdict['starttime']
     fmt = "WID2 %4d/%02d/%02d %02d:%02d:%06.3f %-5s %-3s %-4s %-3s %8d " + \
@@ -236,11 +186,6 @@ def writeHeader(f, headdict):
 def uncompress_CM6(f, n_samps):
     """
     Uncompress n_samps of CM6 compressed data from file pointer fp.
-
-    :type f: file
-    :param f: File Pointer
-    :type n_samps: int
-    :param n_samps: Number of samples
     """
     def read83(cbuf, vptr):  # @UnusedVariable
         line = f.readline()
@@ -251,7 +196,7 @@ def uncompress_CM6(f, n_samps):
         # copy also null termination "\0", that is max 83 bytes
         C.memmove(C.addressof(cbuf.contents), C.addressof(sb), len(line) + 1)
         return C.addressof(sb)
-
+    #
     cread83 = C.CFUNCTYPE(C.c_char_p, C.POINTER(C.c_char), C.c_void_p)(read83)
     if n_samps == 0:
         data = np.empty(0, dtype=np.int32)
@@ -268,17 +213,13 @@ def uncompress_CM6(f, n_samps):
 def compress_CM6(data):
     """
     CM6 compress data
-
-    :type data: :class:`numpy.ndarray`, dtype=int32
-    :param data: the data to write
-    :returns: NumPy chararray containing compressed samples
     """
     data = np.ascontiguousarray(data, np.int32)
     N = len(data)
     count = [0]  # closure, must be container
     # 4 character bytes per int32_t
-    carr = np.zeros(N * 4, dtype=native_str('c'))
-
+    carr = np.zeros(N * 4, dtype=str('c'))
+    #
     def writer(char):
         carr[count[0]] = char
         count[0] += 1
@@ -290,20 +231,13 @@ def compress_CM6(data):
         raise GSEUtiError(msg % ierr)
     cnt = count[0]
     if cnt < 80:
-        return carr[:cnt].view(native_str('|S%d' % cnt))
+        return carr[:cnt].view(str('|S%d' % cnt))
     else:
-        return carr[:(cnt // 80 + 1) * 80].view(native_str('|S80'))
+        return carr[:(cnt // 80 + 1) * 80].view(str('|S80'))
 
 
 def verifyChecksum(fh, data, version=2):
-    """
-    Calculate checksum from data, as in gse_driver.c line 60
-
-    :type fh: file
-    :param fh: File Pointer
-    :type version: int
-    :param version: GSE version, either 1 or 2, defaults to 2.
-    """
+    """ Calculate checksum from data, as in gse_driver.c line 60 """
     chksum_data = clibgse2.check_sum(data, len(data), C.c_int32(0))
     # find checksum within file
     buf = fh.readline()
@@ -329,56 +263,18 @@ def verifyChecksum(fh, data, version=2):
 
 
 def read(f, verify_chksum=True):
-    """
-    Read GSE2 file and return header and data.
-
-    Currently supports only CM6 compressed GSE2 files, this should be
-    sufficient for most cases. Data are in circular frequency counts, for
-    correction of calper multiply by 2PI and calper: data * 2 * pi *
-    header['calper'].
-
-    :type f: file
-    :param f: Open file pointer of GSE2 file to read, opened in binary mode,
-              e.g. f = open('myfile','rb')
-    :type test_chksum: bool
-    :param verify_chksum: If True verify Checksum and raise Exception if it
-                          is not correct
-    :rtype: Dictionary, :class:`numpy.ndarray`, dtype=int32
-    :return: Header entries and data as numpy.ndarray of type int32.
-    """
+    """ Read GSE2 file and return header and data """
     headdict = readHeader(f)
     data = uncompress_CM6(f, headdict['npts'])
     # test checksum only if enabled
-    if verify_chksum:
-        verifyChecksum(f, data, version=2)
+    if verify_chksum: verifyChecksum(f, data, version=2)
     return headdict, data
 
 
 def write(headdict, data, f, inplace=False):
-    """
-    Write GSE2 file, given the header and data.
-
-    Currently supports only CM6 compressed GSE2 files, this should be
-    sufficient for most cases. Data are in circular frequency counts, for
-    correction of calper multiply by 2PI and calper:
-    data * 2 * pi * header['calper'].
-
+    """ Write GSE2 file, given the header and data.
     Warning: The data are actually compressed in place for performance
     issues, if you still want to use the data afterwards use data.copy()
-
-    :note: headdict dictionary entries C{'datatype', 'n_samps',
-           'samp_rate'} are absolutely necessary
-    :type data: :class:`numpy.ndarray`, dtype=int32
-    :param data: Contains the data.
-    :type f: file
-    :param f: Open file pointer of GSE2 file to write, opened in binary
-              mode, e.g. f = open('myfile','wb')
-    :type inplace: bool
-    :param inplace: If True, do compression not on a copy of the data but
-                    on the data itself --- note this will change the data
-                    values and make them therefore unusable
-    :type headdict: dict
-    :param headdict: ObsPy Header
     """
     N = len(data)
     #
@@ -511,6 +407,3 @@ def compile_STA2(stats):
             warnings.warn(msg)
     return line.encode('ascii', 'strict')
 
-
-if __name__ == '__main__':
-    doctest.testmod(exclude_empty=True)

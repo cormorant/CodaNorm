5d4a4df5693f15df9e4236876f2d1c9251d6103f
Peter Forever <crmpeter@gmail.com>
2015-06-04 10:35:32 +0800

    Doing mygse (get code from mymseed.obspycore)

--- baikal2gse.py
+++ baikal2gse.py
@@ -1,12 +1,17 @@
 #coding: utf-8
 """
-Convert baikal file to seisan format using obspy
+Convert baikal file to seisan format
 """
+__version__="0.0.1"
+COMPANY_NAME = 'GIN'
+APP_NAME = 'baikal2gse'
+
 import os
 import sys
 import datetime
 from obspy.core import Trace, Stream, UTCDateTime
 import numpy as np
+import argparse
 
 from baikal import BaikalFile
 
@@ -80,13 +85,67 @@ def write_seisan(filename):
     stream.write(os.path.join(path, name), format='GSE2')
 
 
+def main(args):
+    """ convert files (or directories) to gse2 format """
+    # parse arguments
+    dirs = args.dirs
+    outdir = args.outdir
+    if not os.path.exists(outdir): os.makedirs(outdir)
+    '''
+    # times
+    starttime = args.start
+    endtime = args.end
+    # convert to UTCDateTime
+    starttime = str2UTCDateTime(starttime)
+    # decrease starttime in 1 minute?
+    endtime = str2UTCDateTime(endtime)
+    '''
+    # create Stream
+    stream = Stream()
+    # read mseed files
+    for filename in files:
+        stream += readMSEED(filename, format="MSEED", starttime=starttime, endtime=endtime)
+    # may be there no times: then align stream!
+    starttime = max([trace.stats.starttime for trace in stream])
+    endtime = min([trace.stats.endtime for trace in stream])
+    stream.trim(starttime=starttime, endtime=endtime)
+    # write it!
+    print "Writing {network}_{station}_{location}({starttime}-{endtime})".format(**stream[0].stats)
+    result = write_baikal(stream, args)
+
+
 if __name__ == "__main__":
-    # в указанной папке конвертировать все файлы
-    # сохраняя результат в соотв папке gse2
-    Dir = "data"
-    for root, dirnames, filenames in os.walk(Dir):
-        for filename in filenames:
-            filename = os.path.join(root, filename)
-            print("Converting file %s" % filename)
-            try: write_seisan(filename)
-            except: print("Error writing file %s" % filename)
+    #===========================================================================
+    # parsers
+    parser = argparse.ArgumentParser()
+    # общие парсеры
+    parser.add_argument('-V', '--version', action='version', 
+        version='%(prog)s.' + __version__)
+    # конвертировать эти файлы
+    parser.add_argument("dirs", nargs='+', help="directories to convert")
+    # куда выходные файлы
+    parser.add_argument("-o", "--outdir", dest="outdir", default="gse2",
+        help="path for output data (default is \"gse2\")")
+    # start from
+    #parser.add_argument("-s", "--start", dest="start",
+    #    help="start time (format \"YYYY-mm-ddTHH:MM:SS\")"
+    #    " like 2015-05-28T07:12:56")
+    # end time
+    #parser.add_argument("-e", "--end", dest="end",
+    #    help="end time (format YYYY-mm-ddTHH:MM:SS)")
+    #== end of parsing parameters
+    args = parser.parse_args()
+    print args
+    sys.exit(0)
+    #===========================================================================
+    #
+    #TODO: проверять, ведь нельзя считывать и записывать одну и ту же папку
+    #
+    #=== start job
+    # convert files in arguments, into one file in Baikal-5 format
+    try:
+        main(args)
+    except (ValueError,) as e:
+        print("Error: {}".format(e))
+        sys.exit(1)
+
---
+++ mygse/obspycore/attribdict.py
@@ -0,0 +1,125 @@
+# -*- coding: utf-8 -*-
+from __future__ import division
+import collections
+import copy
+
+
+class AttribDict(collections.MutableMapping):
+    """
+    A class which behaves like a dictionary.
+    """
+    defaults = {}
+    readonly = []
+
+    def __init__(self, *args, **kwargs):
+        """
+        An AttribDict can be initialized in two ways. It can be given an
+        """
+        # set default values directly
+        self.__dict__.update(self.defaults)
+        # use overwritable update method to set arguments
+        self.update(dict(*args, **kwargs))
+
+    def __repr__(self):
+        return "%s(%s)" % (self.__class__.__name__, self.__dict__)
+
+    def __getitem__(self, name, default=None):
+        try:
+            return self.__dict__[name]
+        except KeyError:
+            # check if we got any default value given at class level
+            if name in self.defaults:
+                return self.defaults[name]
+            # if both are missing check for a given default value
+            if default is None:
+                raise
+            return default
+
+    def __setitem__(self, key, value):
+        if key in self.readonly:
+            msg = 'Attribute "%s" in %s object is read only!'
+            raise AttributeError(msg % (key, self.__class__.__name__))
+
+        if isinstance(value, collections.Mapping) and \
+           not isinstance(value, AttribDict):
+            self.__dict__[key] = AttribDict(value)
+        else:
+            self.__dict__[key] = value
+
+    def __delitem__(self, name):
+        del self.__dict__[name]
+
+    def __getstate__(self):
+        return self.__dict__
+
+    def __setstate__(self, adict):
+        # set default values
+        self.__dict__.update(self.defaults)
+        # update with pickle dictionary
+        self.update(adict)
+
+    def __getattr__(self, name, default=None):
+        """
+        Py3k hasattr() expects an AttributeError no KeyError to be
+        raised if the attribute is not found.
+        """
+        try:
+            return self.__getitem__(name, default)
+        except KeyError as e:
+            raise AttributeError(e.args[0])
+
+    __setattr__ = __setitem__
+    __delattr__ = __delitem__
+
+    def copy(self):
+        return copy.deepcopy(self)
+
+    def __deepcopy__(self, *args, **kwargs):  # @UnusedVariable
+        ad = self.__class__()
+        ad.update(copy.deepcopy(self.__dict__))
+        return ad
+
+    def update(self, adict={}):
+        for (key, value) in adict.items():
+            if key in self.readonly:
+                continue
+            self.__setitem__(key, value)
+
+    def _pretty_str(self, priorized_keys=[], min_label_length=16):
+        """
+        Return better readable string representation of AttribDict object.
+
+        :type priorized_keys: list of str, optional
+        :param priorized_keys: Keywords of current AttribDict which will be
+            shown before all other keywords. Those keywords must exists
+            otherwise an exception will be raised. Defaults to empty list.
+        :type min_label_length: int, optional
+        :param min_label_length: Minimum label length for keywords. Defaults
+            to ``16``.
+        :return: String representation of current AttribDict object.
+        """
+        keys = list(self.keys())
+        # determine longest key name for alignment of all items
+        try:
+            i = max(max([len(k) for k in keys]), min_label_length)
+        except ValueError:
+            # no keys
+            return ""
+        pattern = "%%%ds: %%s" % (i)
+        # check if keys exist
+        other_keys = [k for k in keys if k not in priorized_keys]
+        # priorized keys first + all other keys
+        keys = priorized_keys + sorted(other_keys)
+        head = [pattern % (k, self.__dict__[k]) for k in keys]
+        return "\n".join(head)
+
+    def __iter__(self):
+        return iter(self.__dict__)
+
+    def __len__(self):
+        return len(self.__dict__)
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod(exclude_empty=True)
